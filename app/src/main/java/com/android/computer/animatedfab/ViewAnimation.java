63a46f74f93b0e298bd6ddffd73ab0dc37d89883
Segun Francis <francis4dblues@gmail.com>
2019-08-07 05:22:04 +0100

    add animation methods

--- app/src/main/java/com/android/computer/animatedfab/ViewAnimation.java
+++ app/src/main/java/com/android/computer/animatedfab/ViewAnimation.java
@@ -16,4 +16,38 @@ public class ViewAnimation {
                 }).rotation(rotate ? 135f : 0f);
         return rotate;
     }
+
+    public static void showIn(final View view) {
+        view.setVisibility(View.VISIBLE);
+        view.setAlpha(0f);
+        view.setTranslationY(view.getHeight());
+        view.animate().setDuration(200).translationY(0)
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                    }
+                }).alpha(1f)
+                .start();
+    }
+
+    public static void showOut(final View view) {
+        view.setVisibility(View.VISIBLE);
+        view.setAlpha(1f);
+        view.setTranslationY(0);
+        view.animate().setDuration(200).translationY(view.getHeight())
+                .setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        super.onAnimationEnd(animation);
+                    }
+                }).alpha(0f)
+                .start();
+    }
+
+    public static void init(final View view) {
+        view.setVisibility(View.GONE);
+        view.setTranslationY(view.getHeight());
+        view.setAlpha(0f);
+    }
 }
